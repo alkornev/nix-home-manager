57ae0bd726f5f846a54aeba484fd07ed51ad0b23
Aleksei Kornev <al.a.kornev@gmail.com>
2026-02-05 00:51:45 +0100

    Add string interpolated value for username

--- home.nix
+++ home.nix
@@ -1,19 +1,20 @@
-{ lib, pkgs, ... }:
-{
+{ lib, pkgs, ... }: let 
+  username = "aalkornev";
+in {
   home = {
     packages = with pkgs; [
       hello
     ];
     
-    username = "aalkornev";
-    homeDirectory = "/home/aalkornev";
+    inherit username; 
+    homeDirectory = "/home/${username}";
     
     file = {
       "hello.txt" = {
         text = ''
           #!/usr/bin/env bash
 
-          echo "Hello, World!"
+          echo "Hello, ${username}!"
           echo '*slaps roof* This script can fit so many lines in it'
         '';
         executable = true;
