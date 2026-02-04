5a09b9ac81f22e2bf2b794ef42a59055a8210629
Aleksei Kornev <al.a.kornev@gmail.com>
2026-02-05 00:47:34 +0100

    add multiline script

--- home.nix
+++ home.nix
@@ -8,6 +8,17 @@
     username = "aalkornev";
     homeDirectory = "/home/aalkornev";
     
+    file = {
+      "hello.txt" = {
+        text = ''
+          #!/usr/bin/env bash
+
+          echo "Hello, World!"
+          echo '*slaps roof* This script can fit so many lines in it'
+        '';
+        executable = true;
+      };
+    };
 
     stateVersion = "25.11";
   };
