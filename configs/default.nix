a09454bc39b3615785bdbb418ac6c8fdf012597a
Aleksei Kornev <al.a.kornev@gmail.com>
2026-02-09 19:44:08 +0100

    add geocode example

---
+++ configs/default.nix
@@ -0,0 +1,58 @@
+{ pkgs, lib, config, ... }:  {
+  imports = [
+    ./marker.nix
+  ];
+
+  options = {
+    scripts.output = lib.mkOption {
+      type = lib.types.package;
+    };
+   
+    scripts.geocode = lib.mkOption {
+      type = lib.types.package;
+    }; 
+
+    requestParams = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+    };
+ 
+    map = {
+      zoom = lib.mkOption {
+        type = lib.types.nullOr lib.types.int;
+        default = 5;
+      };
+      center = lib.mkOption {
+        type = lib.types.nullOr lib.types.str;
+        default = "switzerland";
+      };
+    };
+
+};
+
+
+  config = {
+    scripts.output = pkgs.writeShellApplication {
+      name = "map";
+      runtimeInputs = with pkgs; [ curl feh ];
+      text = ''
+        ${./map.sh} ${lib.concatStringsSep " " config.requestParams} | feh -
+      '';
+    };
+
+
+    scripts.geocode = pkgs.writeShellApplication {
+      name = "geocode";
+      runtimeInputs = with pkgs; [ curl jq ];
+      text = ''exec ${./geocode.sh} "$@"'';
+    };
+
+
+    requestParams = [
+      "size=640x640"
+      "scale=2"
+      (lib.mkIf (config.map.zoom != null) "zoom=${toString config.map.zoom}")
+      (lib.mkIf (config.map.center != null) "center=\"$(${config.scripts.geocode}/bin/geocode ${lib.escapeShellArg config.map.center})\"")
+    ];
+  };
+}
+
---
+++ configs/marker.nix
@@ -0,0 +1,70 @@
+{ lib, config, ... }: 
+let
+  markerType = lib.types.submodule {
+    options = {
+      location = lib.mkOption {
+        type = lib.types.nullOr lib.types.str;
+        default = null;
+      };
+
+      style.label = lib.mkOption {
+        type = lib.types.nullOr
+          (lib.types.strMatching "[A-Z0-9]");
+        default = null;
+      };
+    };
+  };
+
+  userType = lib.types.submodule {
+    options = {
+      departure = lib.mkOption {
+        type = markerType;
+        default = {};
+      };
+    };
+  };
+in {
+  options = {
+   users = lib.mkOption {
+     type = lib.types.attrsOf userType;
+   };
+
+    map.markers = lib.mkOption {
+      type = lib.types.listOf markerType;
+    };
+  };
+
+  config = {
+    users.a.departure.location = "new york";
+    users.b.departure.location = "switzerland";
+
+    map.markers = lib.filter
+      (marker: marker.location != null)
+      (lib.concatMap (user: [
+         user.departure
+      ]) (lib.attrValues config.users));
+
+    map.center = lib.mkIf 
+      (lib.length config.map.markers >= 1)
+      null;
+
+    map.zoom = lib.mkIf
+      (lib.length config.map.markers >= 2)
+      null;
+
+    requestParams = let
+      paramForMarker = marker:
+        let
+          attributes = 
+            lib.optional (marker.style.label != null)
+            "label:${marker.style.label}"
+            ++ [
+              "$(${config.scripts.geocode}/bin/geocode ${
+                lib.escapeShellArg marker.location
+              })"
+            ];
+        in "markers=\"${lib.concatStringsSep "|" attributes}\"";
+      in
+        builtins.map paramForMarker config.map.markers;
+  };
+}
