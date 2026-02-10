f2d4f4f14d3c28d45b2ef7a4f1f2396bb6784133
Aleksei Kornev <al.a.kornev@gmail.com>
2026-02-11 00:01:52 +0100

    update starship.nix

--- modules/starship.nix
+++ modules/starship.nix
@@ -1,5 +1,21 @@
+{ lib, ... }: 
 {
   programs.starship = {
     enable = true;
+    
+    settings = {
+      format = lib.concatStrings [
+        "status"
+	"$line_break"
+	"$python"
+        "$time"
+	"$directory"
+	"$git_branch"
+	"$git_commit"
+	"$git_state"
+	"$git_status"
+	"$line_break"
+      ];
+    };
   };
 }
