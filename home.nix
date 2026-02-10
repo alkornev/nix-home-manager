d809abaf9f1dd0d8b5931f063863a4236df775c2
Aleksei Kornev <al.a.kornev@gmail.com>
2026-02-10 23:02:19 +0100

    move programs configuration to modules

--- home.nix
+++ home.nix
@@ -1,61 +1,22 @@
 { lib, pkgs, ... }: let 
   username = "aalkornev";
 in {
+
+  imports = [
+    ./modules/zsh.nix
+    ./modules/git.nix
+  ];
+
   home = {
-    packages = with pkgs; [
-      hello
-      home-manager
-      htop
-      feh
+    packages = [
+      pkgs.home-manager
+      pkgs.htop
+      pkgs.neovim
     ];
     
     inherit username; 
     homeDirectory = "/home/${username}";
    
     stateVersion = "25.11";
-  };
-
-  programs.git = {
-    enable = true;
-    settings.user.name = "Aleksei Kornev";
-    settings.user.email = "al.a.kornev@gmail.com";
-  };
-  
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-
-    oh-my-zsh = {
-      enable = true;
-      plugins = [ "git" "sudo" ];
-      theme = "robbyrussell";
-    };
-    antidote = {
-      enable = true;
-      plugins = [
-        "zsh-users/zsh-syntax-highlighting"
-        "zsh-users/zsh-history-substring-search"
-        "zsh-users/zsh-autosuggestions"
-        "zsh-users/zsh-completions"
-      ];
-    };
-  };
-
-  programs.vim = {
-    enable = true;
-
-    settings = {
-      relativenumber = true;
-    };
-    
-    plugins = with pkgs.vimPlugins; [
-      vim-nix
-    ];
-    
-    extraConfig = ''
-      syntax on
-    '';
   };
 }
