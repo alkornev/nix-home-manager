af79b0c6c6bb0963a1b2539b990e04c4ab5c19bf
Aleksei Kornev <al.a.kornev@gmail.com>
2026-05-11 19:53:13 +0200

    enable amd kvm + libvirt

--- hosts/desktop/virtualization.nix
+++ hosts/desktop/virtualization.nix
@@ -21,15 +21,43 @@
 
   virtualisation.containers.registries.search = [ "docker.io" ];
 
+  boot.kernelModules = [ "kvm-amd" ];
+
+  boot.extraModprobeConfig = ''
+    options kvm_amd nested=1
+  '';
+
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = false;
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [ pkgs.OVMFFull.fd ];
+      };
+    };
+  };
+
+  programs.virt-manager.enable = true;
+
+  virtualisation.spiceUSBRedirection.enable = true;
+
   environment.systemPackages = with pkgs; [
     dive # look into docker image layers
     podman-tui # status of containers in the terminal
+    podman-desktop # GUI for managing containers / pods / images
     docker-compose # start group of containers for dev
     #podman-compose # start group of containers for dev
+    distrobox
+    virtiofsd
+    spice-gtk
   ];
 
   hardware.nvidia-container-toolkit.enable = true;
   users.users.${username} = {
+    extraGroups = [ "kvm" "libvirtd" ];
     subUidRanges = [
       {
         startUid = 100000;
